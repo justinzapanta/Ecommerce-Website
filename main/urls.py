ca78103485897c2f5b403b7f18fe426d64b233b5
justinzapanta <zapantajustin5@gmail.com>
2024-04-01 23:40:19 +0800

    Done Displaying Product from database

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('shop/', views.shop, name='shop'),
     path('signin/', include('user_authentication.urls')),
     path('seller/', include('seller.urls'))
-]
+    
+] + static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
