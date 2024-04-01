ced9a0a05ea5712e1b5669932f06934908cec2e6
justinzapanta <zapantajustin5@gmail.com>
2024-04-01 20:19:51 +0800

    Seller Database Created

--- seller/models.py
+++ seller/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     item_id = models.AutoField(primary_key=True)
+    item_image = models.ImageField(upload_to='product/images', default='Null')
     item_name = models.CharField(max_length=200)
     item_price = models.IntegerField()
     item_quantity = models.ImageField()
@@ -13,3 +14,10 @@ class Cart(models.Model):
     item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
     cart_quantity = models.IntegerField()
     cart_is_checkedout = models.BooleanField()
+
+
+class Transaction(models.Model):
+    transaction_id = models.AutoField(primary_key=True)
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    transaction_total_price = models.IntegerField()
+    transaction_date = models.DateTimeField(auto_now_add=True)
