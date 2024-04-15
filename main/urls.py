d1cb3b7677781b89d315ca2ff79fe89a448e426e
justinzapanta <zapantajustin5@gmail.com>
2024-04-15 13:30:53 +0800

    renaming my file

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-from .endpoint import cart_endpoint, transaction_endpoint, profile_endpoint
+from .backend import cart_backend, profile_backend, transaction_backend
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -13,15 +13,15 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 
     #Cart End Point
-    path('api/cart/add-item/', cart_endpoint.add_item),
-    path('api/cart/delete-item/', cart_endpoint.delete_item),
-    path('api/cart/update-quantity/', cart_endpoint.update_quantity),
-    path('api/cart/place-order/', cart_endpoint.place_order),
+    path('api/cart/add-item/', cart_backend.add_item),
+    path('api/cart/delete-item/', cart_backend.delete_item),
+    path('api/cart/update-quantity/', cart_backend.update_quantity),
+    path('api/cart/place-order/', cart_backend.place_order),
 
     #Transaction End Point
-    path('api/transaction/new-transaction/', transaction_endpoint.new_transaction, name='new transaction'),
-    path('api/transaction/get-item', transaction_endpoint.get_item),
+    path('api/transaction/new-transaction/', transaction_backend.new_transaction, name='new transaction'),
+    path('api/transaction/get-item', transaction_backend.get_item),
 
     #Profile End Point
-    path('api/profile/update', profile_endpoint.change_personal_info),
+    path('api/profile/update', profile_backend.change_personal_info),
 ] + static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
